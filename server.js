da845ccaeba7fb662ffa042047865c9e5de11212
abdelaziz <abdelazizt320@gmail.com>
2019-11-19 12:42:10 +0200

    making the  main structure of caclulator services

---
+++ server.js
@@ -0,0 +1,22 @@
+const app = require('./app')
+const http = require('http')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port} 🔥`));
+
